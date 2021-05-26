84d3dc47ab6405ca65616d09d2ac6613c96c3342
Senath99 <senathweerasingha@gmail.com>
2021-05-26 19:13:50 +0530

    Final commit Message

--- src/main/java/com/example/demo/Controller/StorageController.java
+++ src/main/java/com/example/demo/Controller/StorageController.java
@@ -1,14 +1,20 @@
 package com.example.demo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.service.StorageService;
+
+import software.amazon.awssdk.http.Header;
 
 @RestController
 public class StorageController {
@@ -18,10 +24,27 @@ public class StorageController {
 	
 	@PostMapping("/upload")
 	public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file){
-		
 		return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+	}
+	
+	@GetMapping("/download/{fileName}")
+	public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName){
 		
+		
+		byte[] data = service.downloadFile(fileName);
+		ByteArrayResource byteArrayResource = new ByteArrayResource(data);
+		return ResponseEntity
+				.ok()
+				.contentLength(data.length)
+				.header("Content-type", "application/octet-stream")
+				.header("Content-disposition", "attachment; filename=\" "+ fileName + "\"")
+				.body(byteArrayResource);
 	}
 
+	@DeleteMapping("/delete/{fileName}")
+	public ResponseEntity<String> deleteFile(@PathVariable String fileName){
+		return new ResponseEntity<>(service.deleteFile(fileName),HttpStatus.OK);
+	}
+	
 	
 }
--- src/main/java/com/example/demo/service/StorageService.java
+++ src/main/java/com/example/demo/service/StorageService.java
@@ -5,13 +5,21 @@ import static com.example.demo.Config.StorageConfig.getS3Client;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectResponse;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.utils.IoUtils;
 
 @Service
 @Slf4j
@@ -26,12 +34,38 @@ public class StorageService {
 		
 		
 	}
+	
+	public byte[] downloadFile(String fileName){
+		
+		GetObjectRequest getObjectRequest = null;
+		//getObject
+		getObjectRequest =  GetObjectRequest.builder().bucket("senath").key(fileName).build();
+		//converting to inputstream
+		ResponseInputStream inputStream = getS3Client().getObject(getObjectRequest);
+		
+		try {
+			byte[] content = IoUtils.toByteArray(inputStream);
+			return content;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return null;
+	
+	}	
 
+
+	
+	public String deleteFile(String fileName) {
+		
+		 DeleteObjectRequest deleteObjectRequest = null;
+		 deleteObjectRequest = DeleteObjectRequest.builder().bucket("senath").key(fileName).build();
+		 DeleteObjectResponse deleteObjectResponse = getS3Client().deleteObject(deleteObjectRequest);
+		 return fileName + " Deleted !!";
+		
+	}
 	//Convert multipartfile to file
-	
-	
-	
-	
 	private File convertMultiPartFiletoFile(MultipartFile file) {
 		
 		File convertedFile = new File(file.getOriginalFilename());
